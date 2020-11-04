2e72230f48fc8a096dcb5e4c15da436b11a60acb
unknown <henry@novap.legal>
2020-11-03 22:09:21 -0500

    Fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }, { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  {
+    path: "blog",
+    loadChildren: () => import("./blog/blog.module").then((m) => m.BlogModule),
+  },
+  {
+    path: "",
+    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
+  },
+  {
+    path: "contact",
+    loadChildren: () =>
+      import("./contact/contact.module").then((m) => m.ContactModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
